19ed64de9b4cc320eb4ca6a2a3458ee72a187bc1
Jarod <schuppjarod@gmail.com>
2022-09-05 12:55:59 -0400

    Colors Class added

---
+++ src/COLORS.java
@@ -0,0 +1,28 @@
+public enum COLORS {
+        Blue(1),
+        Orange(2), 
+        Green(3), 
+        Brown(4), 
+        Slate(4), 
+        White(5), 
+        Red(6), 
+        Black(7), 
+        Yellow(8), 
+        Violet(9), 
+        Rose(10), 
+        Aqua(11);
+
+        public final int Value;
+
+        private COLORS(int value)
+        {
+            Value = value;
+        
+        }
+
+        public static COLORS Blue() {
+                return null;
+        }
+}
+
+
