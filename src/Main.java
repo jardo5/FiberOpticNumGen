435db6d99d1cd62bf475acb8bab828abb6dd1110
Jarod <schuppjarod@gmail.com>
2022-09-06 18:35:53 -0400

    Removed unneccesary array

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,18 @@ public class Main {
         System.out.println("Enter Number...");
 
         String[] fiberColors = {"Blue", "Orange", "Green", "Brown", "Slate", "White", "Red", "Black", "Yellow", "Violet", "Rose", "Aqua"};
-        String[] tubeColors = {"Blue", "Orange", "Green", "Brown", "Slate", "White", "Red", "Black", "Yellow", "Violet", "Rose", "Aqua"};
 
-        int input = inputNum.nextInt(); // Read user input for Row
+        int numInput = inputNum.nextInt(); // Read user input for Row
         
-         if (input < 145) { 
-            int rowNum = (input - 1) % fiberColors.length;   // Row
+         if (numInput < 145) { 
+            int rowNum = (numInput - 1) % fiberColors.length;   // Row
             System.out.println("The Fiber Color is.." + fiberColors[rowNum]);
             
-            int colNum = (input - 1) / fiberColors.length;                  // Column
+            int colNum = (numInput - 1) / fiberColors.length;                  // Column
             System.out.println("The Tube Color is.." + fiberColors[colNum]);
          } else {
             System.out.println("Invalid input"); // Invalid input
          }
          
-        
-        
-
     }      
 }
