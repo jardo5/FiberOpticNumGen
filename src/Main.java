2fbbd66f31d363bc9cf1d241e4a175f49943dba0
Jarod <schuppjarod@gmail.com>
2022-09-06 20:20:52 -0400

    Added Scanner for Color to Num

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
+
+
 
 public class Main {
     public static void main(String[] args) {
-        Scanner inputNum = new Scanner(System.in);  // User inputs Intager
-        System.out.println("Enter Number...");
+        Scanner inputNum = new Scanner(System.in);  // User inputs Int
+        System.out.println("Only Supporting 144 Count!");
+        System.out.println("Enter Fiber Number...");
 
-        String[] fiberColors = {"Blue", "Orange", "Green", "Brown", "Slate", "White", "Red", "Black", "Yellow", "Violet", "Rose", "Aqua"};
+        String[] fiberColors = {"Blue", "Orange", "Green", "Brown", "Slate", "White", "Red", "Black", "Yellow", "Violet", "Rose", "Aqua"}; //12 Fiber colors
 
-        int numInput = inputNum.nextInt(); // Read user input for Row
-        
+        int numInput = inputNum.nextInt(); // Read user input for Number
          if (numInput < 145) { 
             int rowNum = (numInput - 1) % fiberColors.length;   // Row
             System.out.println("The Fiber Color is.." + fiberColors[rowNum]);
@@ -19,6 +21,23 @@ public class Main {
          } else {
             System.out.println("Invalid input"); // Invalid input
          }
-         
-    }      
+
+
+
+
+         Scanner inputRow = new Scanner(System.in);  // User inputs String Row (Fiber Color)
+         System.out.println("Enter Fiber color"); 
+
+         String rowColor = inputRow.nextLine(); // Read user input for Row
+
+
+
+    }
+
 }
+
+
+
+         // Arrays.sort(fiberColors);  Sort fiberColors array for binarySearch
+
+         // System.out.println(numRow + " Was Found at " + Arrays.binarySearch(fiberColors, numRow));
