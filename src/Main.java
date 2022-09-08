4e095f55948542d02df56b6b13d3a84bd53910c9
Jarod <schuppjarod@gmail.com>
2022-09-08 19:15:19 -0400

    Added inverse calculations to recieve fiber number

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,10 @@ public class Main {
 
         String[] fiberColors = {"Blue", "Orange", "Green", "Brown", "Slate", "White", "Red", "Black", "Yellow", "Violet", "Rose", "Aqua"}; // 12 Fiber colors
 
+
+
+         // Number Conversion
+
         int numInput = inputNum.nextInt(); // Read user input for Number
          if (numInput < 145) { 
             int rowNum = (numInput - 1) % fiberColors.length;   // Row
@@ -24,36 +28,46 @@ public class Main {
          }
 
 
+         // Row Conversion
 
          Scanner inputRow = new Scanner(System.in);  // User inputs String Row (Fiber Color)
          System.out.println("Enter Fiber color"); 
 
          List<String> fiberList = Arrays.asList(fiberColors); // Convert fiberColors to ArrayList
          System.out.println("Supported Colors: " + fiberList); //List all fiber colors
+
+         int rowOutput = 0;
          
          String input = inputRow.next();
          if (fiberList.contains(input)) { // Check if inputRow is equal to a fiber color
             int rowConversion = fiberList.indexOf(input); // Convert from input to index of fiberList
-            int rowOutput = rowConversion;
+            rowOutput = rowConversion;
             System.out.println("output: " + rowOutput);
             } else {
                System.out.println("Invalid input");
             }
          
 
-
+         // Column Conversion
 
          Scanner inputColumn = new Scanner(System.in);  // User inputs String Column (Tube Color)
          System.out.println("Enter Tube color");
 
+         int columnOutput = 0;
+
          String columnInput = inputRow.next();
          if (fiberList.contains(input)) { // Check if inputRow is equal to a fiber color
             int columnConversion = fiberList.indexOf(columnInput); // Convert from input to index of fiberList
-            int columnOutput = columnConversion;
+            columnOutput = columnConversion;
             System.out.println("output: " + columnOutput);
             } else {
                System.out.println("Invalid input");
             }
+
+            int numOutput = (columnOutput*fiberColors.length) + (rowOutput+1 % fiberColors.length); // Inverse colNum and rowNum calculations
+            System.out.println(numOutput);
+
+
 
     }
 
