59d5f79a49962e9f8164ce3268ee499bc8be7db4
Jarod <schuppjarod@gmail.com>
2022-09-07 21:31:32 -0400

    Y-Axis Color to Arraylist Index conversion

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("Only Supporting 144 Count!");
         System.out.println("Enter Fiber Number...");
 
-        String[] fiberColors = {"Blue", "Orange", "Green", "Brown", "Slate", "White", "Red", "Black", "Yellow", "Violet", "Rose", "Aqua"}; //12 Fiber colors
+        String[] fiberColors = {"Blue", "Orange", "Green", "Brown", "Slate", "White", "Red", "Black", "Yellow", "Violet", "Rose", "Aqua"}; // 12 Fiber colors
 
         int numInput = inputNum.nextInt(); // Read user input for Number
          if (numInput < 145) { 
@@ -25,26 +25,35 @@ public class Main {
 
 
 
-
          Scanner inputRow = new Scanner(System.in);  // User inputs String Row (Fiber Color)
          System.out.println("Enter Fiber color"); 
 
-         List<String> fiberList = Arrays.asList(fiberColors);
-         System.out.println("Supported Colors: " + fiberList);
+         List<String> fiberList = Arrays.asList(fiberColors); // Convert fiberColors to ArrayList
+         System.out.println("Supported Colors: " + fiberList); //List all fiber colors
          
          String input = inputRow.next();
-         if (fiberList.contains(input)) {
-            int conversion = fiberList.indexOf(input);
-            int output = conversion;
-            System.out.println("output: " + output);
-
-
-
-
+         if (fiberList.contains(input)) { // Check if inputRow is equal to a fiber color
+            int rowConversion = fiberList.indexOf(input); // Convert from input to index of fiberList
+            int rowOutput = rowConversion;
+            System.out.println("output: " + rowOutput);
+            } else {
+               System.out.println("Invalid input");
             }
          
 
 
+
+         Scanner inputColumn = new Scanner(System.in);  // User inputs String Column (Tube Color)
+         System.out.println("Enter Tube color");
+
+         String columnInput = inputRow.next();
+         if (fiberList.contains(input)) { // Check if inputRow is equal to a fiber color
+            int columnConversion = fiberList.indexOf(columnInput); // Convert from input to index of fiberList
+            int columnOutput = columnConversion;
+            System.out.println("output: " + columnOutput);
+            } else {
+               System.out.println("Invalid input");
+            }
 
     }
 
